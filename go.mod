6bc0786749628d02521ecbba41a8aa5a2085dd33
Dysproz <krasuski.szymon.piotr@gmail.com>
2021-03-08 09:53:18 +0100

    Add first version of crawling script

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module contributors-crawl
 
 go 1.16
 
-require github.com/google/go-github/v33 v33.0.0
+require (
+	github.com/avast/retry-go v3.0.0+incompatible
+	github.com/dnlo/struct2csv v0.0.0-20190928115744-2f584471b24e
+	github.com/google/go-github/v33 v33.0.0
+	github.com/iancoleman/strcase v0.1.3 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	// "flag"
+	"context"
+	"encoding/csv"
+	"flag"
 	"log"
 	"os"
-	"fmt"
-	"context"
 	"time"
+
+	retry "github.com/avast/retry-go"
+	"github.com/dnlo/struct2csv"
 	"github.com/google/go-github/v33/github"
 )
 
@@ -14,43 +17,115 @@ var ctx = context.Background()
 
 // Contributor is a struct with information about Contributor for CSV processing
 type Contributor struct {
-	Repository string
-	AuthorLogin string
-	AuthorName string
-	AuthorEmail string
+	Repository         string
+	AuthorLogin        string
 	TotalContributions int
 	RangeContributions int
-	RangeAdditions int
-	RandeDeletions int
+	RangeAdditions     int
+	RandeDeletions     int
 }
+
+// ISOTimeLayout is a sample layout used for time date parse
+var ISOTimeLayout = "2006-01-02"
 
 func main() {
-	// organization := flag.String("organization", "", "GitHub organization/profile to gather projects from.")
-	// // startDate := flag.String("start-date", "", "Start date from which contributions should be counted. DD-MM-YYYY")
-	// // endDate := flag.String("end-date", "", "End date to which contributions should be counted. DD-MM-YYYY")
-	// flag.Parse()
+	organization := flag.String("organization", "", "GitHub organization/profile to gather projects from.")
+	startDateInput := flag.String("start-date", "", "Start date from which contributions should be counted. YYYY-MM-DD")
+	endDateInput := flag.String("end-date", "", "End date to which contributions should be counted. YYYY-MM-DD")
+	outFile := flag.String("out-file", "contributors-crawl-out.csv", "Path to file where output data in CSV format should be written")
+	repository := flag.String("repository", "", "(OPTIONAL) Specific repository to gather contributions from. If left empty then all repositories in organization will be crawled.")
+	flag.Parse()
 
-	// if *organization == "" {
-	// 	log.Fatal("No organization name specified.")
-	// 	os.Exit(1)
-	// }
-
-	organization := "tungstenfabric"
-	client := github.NewClient(nil)
-	repos, _, err := client.Repositories.ListByOrg(ctx, organization, nil)
-	if err != nil {
-		log.Fatal("Could not list repositories for organization %s", organization)
+	if *organization == "" {
+		log.Fatal("No organization name specified.")
 		os.Exit(1)
 	}
-	fmt.Println(*repos[0].Name)
-	stats, _, err := client.Repositories.ListContributorsStats(ctx, organization, *repos[0].Name)
-	fmt.Println(stats[0].Weeks)
+	startDate, err := time.Parse(ISOTimeLayout, *startDateInput)
+	if err != nil {
+		log.Fatal("Could not parse start (", *startDateInput, ") date string; err: ", err)
+		os.Exit(1)
+	}
+	endDate, err := time.Parse(ISOTimeLayout, *endDateInput)
+	if err != nil {
+		log.Fatal("Could not parse end (", *endDateInput, ") date string; err: ", err)
+		os.Exit(1)
+	}
+
+	client := github.NewClient(nil)
+	var contributions []Contributor
+	var repos []*github.Repository
+	if *repository == "" {
+		repos, _, err = client.Repositories.ListByOrg(ctx, *organization, nil)
+		if err != nil {
+			log.Fatal("Could not list repositories for organization ", *organization, "; err: ", err)
+			os.Exit(1)
+		}
+		log.Println("Successfully gathered repositories for ", *organization, " organization.")
+	} else {
+		repos = []*github.Repository{
+			{
+				Name: repository,
+			},
+		}
+	}
+
+	for _, repo := range repos {
+		var stats []*github.ContributorStats
+		err := retry.Do(
+			func() error {
+				stats, _, err = client.Repositories.ListContributorsStats(ctx, *organization, *repo.Name)
+				if err != nil {
+					log.Println("Request to GitHub failed with err : ", err, "; Trying again in 5 seconds...")
+				}
+				return err
+			}, retry.Delay(5*time.Second),
+		)
+		if err != nil {
+			log.Fatal("Could not list contributors for ", *repo.Name, " repository in organization ", *organization, "; err: ", err)
+			os.Exit(1)
+		}
+		log.Println("Successfully contributors for ", *repo.Name, " repository.")
+
+		for _, stat := range stats {
+			adds, dels, commits := countContributionsInTime(stats[0].Weeks, startDate, endDate)
+			log.Println("Successfully gathered data for contributor ", *stat.Author.Login, " in repository ", *repo.Name)
+			contributions = append(contributions, Contributor{
+				Repository:         *repo.Name,
+				AuthorLogin:        *stat.Author.Login,
+				TotalContributions: *stat.Total,
+				RangeContributions: commits,
+				RangeAdditions:     adds,
+				RandeDeletions:     dels,
+			})
+		}
+	}
+
+	CSVenc := struct2csv.New()
+	CSVData, err := CSVenc.Marshal(contributions)
+	if err != nil {
+		log.Fatal("Could not marshal gathered data to CSV with err: ", err)
+		os.Exit(1)
+	}
+
+	f, err := os.Create(*outFile)
+	if err != nil {
+		log.Fatal("Could not create file ", outFile, " with error: ", err)
+	}
+	defer f.Close()
+
+	CSVwriter := csv.NewWriter(f)
+	if err := CSVwriter.WriteAll(CSVData); err != nil {
+		log.Fatal("Could not write CSV data to file with err: ", err)
+	}
 }
 
-func countContributionsInTime(stats []*github.WeeklyStats, startDate, endDate string) (additions, deletions, commits int) {
-	var additions int
-	var deletions int
-	var commits int
-	time.Parse()
-	for 
+func countContributionsInTime(stats []*github.WeeklyStats, startDate, endDate time.Time) (additions, deletions, commits int) {
+	for _, stat := range stats {
+		if stat.Week.Before(endDate) && stat.Week.After(startDate) {
+			additions += *stat.Additions
+			deletions += *stat.Deletions
+			commits += *stat.Commits
+		}
+	}
+	return additions, deletions, commits
 }
